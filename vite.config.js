0c4a77aa6bdb1ee228600737f816cae397309fdf
Inam Ul Haq Baba <peerzadainamwork@gmail.com>
2026-08-21 14:27:27 +0530

    Fix GitHub Pages typing page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     base: "/JustType/",
     build: {
-        outDir: "docs"
+        outDir: "docs",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                typing: resolve(__dirname, "typing.html")
+            }
+        }
     }
 });
